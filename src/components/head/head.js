a315e22759a9c6904f27600f57a8c31c1d58b854
Cory Henderson <chenderson19899@gmail.com>
2019-08-23 15:36:45 -0700

    header + nav

---
+++ src/components/head/head.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Head = () => (
+
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative|Patua+One&display=swap" rel="stylesheet" />
+  </head>
+)
+
+export default Head;
