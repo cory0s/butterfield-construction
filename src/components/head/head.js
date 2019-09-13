c3f64846cc8aa7c22c5e8467f311dc02aab440fe
Cory Henderson <chenderson19899@gmail.com>
2019-09-12 22:56:06 -0700

    contact

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -4,6 +4,7 @@ const Head = () => (
 
   <head>
     <link href="https://fonts.googleapis.com/css?family=Alegreya+SC|Cinzel+Decorative|Patua+One&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Zilla+Slab&display=swap" rel="stylesheet"/>
   </head>
 )
 
